caa81a576eda7f6eac3064e42023b940e8a93d73
Devin Cooper <devinsiphone@gmail.com>
2019-05-12 21:53:23 +0200

    LINTED "CALCULATOR.RB"

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -21,17 +21,17 @@ end
 
 def operation_to_message(op)
   message = case op
-  when '1'
-    'Adding'
-  when '2'
-    'Subtracting'
-  when '3'
-    'Multiplying'
-  when '4'
-    'Dividing'
-  end
+            when '1'
+              'Adding'
+            when '2'
+              'Subtracting'
+            when '3'
+              'Multiplying'
+            when '4'
+              'Dividing'
+            end
 
-  return message
+  message
 end
 
 prompt("Welcome to Calculator! Enter your name:")
