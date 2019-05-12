8d1f9851786aa2e03240286e220a8d678476ff5a
Devin Cooper <devinsiphone@gmail.com>
2019-05-12 21:42:06 +0200

    edited "calculator.rb" for better input validation

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -10,8 +10,13 @@ def prompt(message)
   puts "=> #{message}"
 end
 
-def valid_number?(num)
-  Integer(num) rescue false
+# def valid_number?(num)
+#   Integer(num) rescue false
+# end
+
+def number?(obj) # Better number input validation
+  obj = obj.to_s unless obj.is_a? String
+  /\A[+-]?\d+(\.[\d]+)?\z/.match(obj)
 end
 
 def operation_to_message(op)
@@ -27,41 +32,41 @@ def operation_to_message(op)
   end
 end
 
-prompt "Welcome to Calculator! Enter your name:"
+prompt("Welcome to Calculator! Enter your name:")
 name = nil
 loop do
   name = gets.chomp
   if name.empty?
-    prompt "Make sure to use a valid name."
+    prompt("Make sure to use a valid name.")
   else
     break
   end
 end
 
-prompt "Hi #{name}!"
+prompt("Hi #{name}!")
 
 loop do # main loop
   number1 = nil
   loop do
-    prompt "What's the first number?"
+    prompt("What's the first number?")
     number1 = gets.chomp
 
-    if valid_number? number1
+    if number?(number1)
       break
     else
-      prompt"Hmm... that doesn't look like a valid number"
+      prompt("Hmm... that doesn't look like a valid number")
     end
   end
 
   number2 = nil
   loop do
-    prompt "What's the second number?"
+    prompt("What's the second number?")
     number2 = gets.chomp
 
-    if valid_number? number2
+    if number?(number2)
       break
     else
-      prompt "Hmm... that doesn't look like a valid number"
+      prompt("Hmm... that doesn't look like a valid number")
     end
   end
 
@@ -72,36 +77,36 @@ loop do # main loop
     3) multiply
     4) divide
   MSG
-  prompt operator_prompt
+  prompt(operator_prompt)
   operator = nil
   loop do
     operator = gets.chomp
     if %w(1 2 3 4).include? operator
       break
     else
-      prompt "Must choose 1, 2, 3, or 4"
+      prompt("Must choose 1, 2, 3, or 4")
     end
   end
 
-  prompt "#{operation_to_message operator} the two numbers..."
+  prompt("#{operation_to_message operator} the two numbers...")
   sleep 2
 
   result = case operator
            when '1'
-             number1.to_i + number2.to_i
+             number1.to_f + number2.to_f
            when '2'
-             number1.to_i - number2.to_i
+             number1.to_f - number2.to_f
            when '3'
-             number1.to_i * number2.to_i
+             number1.to_f * number2.to_f
            when '4'
              number1.to_f / number2.to_f
            end
 
-  prompt "The result is #{result}"
+  prompt("The result is #{result}")
 
-  prompt "Do you want to perform another calculation? (Y for Yes)"
+  prompt("Do you want to perform another calculation? (Y for Yes)")
   answer = gets.chomp
   break unless answer.downcase.start_with? 'y'
 end
 
-prompt "Thank you for using the calculator. Goodbye!"
+prompt("Thank you for using the calculator. Goodbye!")
