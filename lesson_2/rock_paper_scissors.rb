e425c000247344b0f1a1eb0d12e7cdfe72f885ed
Devin Cooper <devinsiphone@gmail.com>
2019-05-18 21:03:09 -0400

    Refactor display_results method
    Linter complained logic was too complex. Created win? method to simplify

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,19 +1,21 @@
 # rock_paper_scissors.rb
 
-VALID_CHOICES = ['rock', 'paper', 'scissors']
+VALID_CHOICES = %w[rock paper scissors]
 
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
+def win?(first, second)
+  (first == 'rock' && second == 'scissors') ||
+    (first == 'paper' && second == 'rock') ||
+    (first == 'scissors' && second == 'paper')
+end
+
 def display_results(player, computer)
-  if (player == 'rock'      && computer == 'scissors') ||
-     (player == 'paper'     && computer == 'rock') ||
-     (player == 'scissors'  && computer == 'paper')
+  if win?(player, computer)
     "You won!"
-  elsif (player == 'rock'     && computer == 'paper') ||
-        (player == 'paper'    && computer == 'scissors') ||
-        (player == 'scissors' && computer == 'rock')
+  elsif win?(computer, player)
     "Computer won!"
   else
     "It's a tie!"
